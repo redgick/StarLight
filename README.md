1c2f944701584fc1ee724ded745812c826f6ed56
Julien Lebunetel <julien@lebunetel.com>
2017-11-20 10:09:21 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # StarLight
 StarLight c'est un jeu accessible à tous qui va mettre une étincelle dans votre trajet de bus !
+
+![Presentation](images/Starlight.jpeg)
+
+Créé lors du hackathon MétroMix 2017 à Rennes, ce jeu est un 1D pong sur une bande de led avec un afficheur pour les scores.
+
+## Matériel nécessaire
+
+ * un Arduino Mega
+ * une bande de LEDs RGB adressables de type *NeoPixel*
+ * un afficheur HT1632c
+ * deux boutons d'arcade
+ * un condensateur électrolytique 1000 μF / 10 V
+ * une résistances 470 Ω
+ * deux résistances 10 kΩ
+ * deux condensateurs céramique 102 (1 nF)
+ * un peu de cable ...
+
+## Montage
+
+À l'aide des résistances 10 kΩ et des condensateurs 1 nF, les boutons sont équipés de filtres anti-rebonds. Le bouton A est raccordé à la broche **10** et le bouton B à la broche **11** :
+
+![Bouton](images/bouton_poussoir.png)
+
+La bande de led est alimentée en 5V et nécessite un condensateur de 1000 μF en parallèlele de l'alimentation.
+On ajoute la résistance 470 Ω en série au signal et on le relie à la broche **3** :
+
+![LEDs](images/leds_Wiring-Diagram.png)
+
+On raccorde la matrice de led à Arduino selon le brochage suivant :
+
+![Matrice](images/dotmatrix_connections.svg)
+
+## Documentation du projet
+
+http://www.wiki-rennes.fr/M%C3%A9troMix_2017/StarLight
---
+++ StarLight.ino
@@ -0,0 +1,229 @@
+/*
+   Firmware du projet "starlight" présenté lors de MétroMix 2017 à Rennes.
+   La documentation est disponible sur http://www.wiki-rennes.fr/M%C3%A9troMix_2017/StarLight
+   Et le code sur https://github.com/jlebunetel/StarLight
+*/
+
+// constantes
+#define JOUEURA 1
+#define JOUEURB 2
+
+// librairie nécessaire au pilotage de la matrice de led HT1632c
+// https://github.com/jlebunetel/HT1632_arduino/tree/ArduinoMega
+#include <HT1632_arduino.h>
+
+// création de l'objet afficheur
+HT1632_arduino afficheur;
+
+// création d'un objet "ecran" qui est un buffeur contenant l'image à afficher sur "afficheur"
+Screen* ecran = new Screen();
+
+// boutons
+const int boutonA = 10;
+const int boutonB = 11;
+
+// librairies pour piloter la bande de led adressable Neopixel
+// Adafruit NeoPixel version 1.1.3
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+// broche sur laquelle est branchée la bande de led
+#define PIN 3
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(238, PIN, NEO_GRB + NEO_KHZ800);
+
+// variables globales
+int taille_raquette = 7;
+int balle_position = 0;
+int balle_direction = 0; // direction = 0 -> de led 0 vers led max et direction = 1 -> de led max vers led 0
+int main_joueur = 0;     // qui a la main ?
+int scoreA = 0;
+int scoreB = 0;
+
+void setup() {
+  // initialisation de la liaison série pour le débogage
+  Serial.begin(115200);
+  Serial.println("demarrage");
+
+  // déclaration des boutons en entrée
+  pinMode(boutonA, INPUT);
+  pinMode(boutonB, INPUT);
+
+  // initialisation de l'afficheur et message de bienvenue
+  afficheur.begin(PWM_10);
+  ecran->setLine("STAR", FIRST, CENTER, ORANGE);
+  ecran->setLine("LIGHT", SECOND, CENTER, ORANGE);
+  afficheur.display(ecran);
+
+  // initialisation de la bande de led
+  strip.begin();
+  strip.show();
+}
+
+
+void loop() {
+  // le jeu est en veille tant qu'aucun joueur n'a pris la main
+  while (main_joueur == 0) {
+    ecran->clear();
+    ecran->setLine("PRESS", FIRST, CENTER, GREEN);
+    ecran->setLine("START", SECOND, CENTER, RED);
+    afficheur.display(ecran);
+
+    // une petite animation pour patienter ...
+    for (uint8_t i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, strip.Color(0, 0, 0));
+    }
+    strip.show();
+
+    // on est en attente d'un appuis sur start
+    while (digitalRead(boutonA) == 1 and digitalRead(boutonB) == 1) {
+      theaterChase(strip.Color(0, 0, 127), 10);
+    }
+
+    // le jeu commence !
+    if (digitalRead(boutonA) == 0) {
+      // A viens d'appuyer
+      main_joueur = JOUEURA;
+    }
+    else {
+      // B viens d'appuyer
+      main_joueur = JOUEURB;
+    }
+
+    // on lance la partie
+    ecran->clear();
+    ecran->setLine("READY?", FIRST, CENTER, ORANGE);
+    afficheur.display(ecran);
+    delay(1000);
+    ecran->setLine("GO !!!", SECOND, CENTER, RED);
+    afficheur.display(ecran);
+    delay(1000);
+  }
+
+
+  // affichage des raquettes
+  for (uint8_t i = 0 ; i < taille_raquette; i++) {
+    // raquette verte
+    strip.setPixelColor(i, strip.Color(0, 64, 0));
+    // raquette rouge
+    strip.setPixelColor(strip.numPixels() - i - 1, strip.Color(64, 0, 0));
+  }
+  strip.show();
+
+
+  // affichage des scores
+  ecran->clear();
+  ecran->setLine("A: " + String(scoreA), FIRST, CENTER, GREEN);
+  ecran->setLine("B: " + String(scoreB), SECOND, CENTER, RED);
+  afficheur.display(ecran);
+
+  // qui commence ?
+  if (main_joueur == JOUEURA) {
+    // le joueur A a la main
+    balle_position = 0;
+    balle_direction = 0;
+    strip.setPixelColor(0, strip.Color(0, 0, 128));
+    strip.show();
+    while (digitalRead(boutonA) == 1) {
+      // on attend un appuis sur le bouton A
+      delay(10);
+    }
+  }
+  else {
+    // le joueur B a la main
+    balle_position = strip.numPixels();
+    balle_direction = 1;
+    strip.setPixelColor(balle_position - 1, strip.Color(0, 0, 128));
+    strip.show();
+    while (digitalRead(boutonB) == 1) {
+      // on attend un appuis sur le bouton B
+      delay(10);
+    }
+  }
+
+  // c'est parti !
+  bool partie_en_cours = true;
+
+  while (partie_en_cours) {
+    // on affiche la balle
+    strip.setPixelColor(balle_position, strip.Color(0, 0, 64));
+    strip.show();
+
+    // on temporise un peu pour ralentir la balle
+    //delay(20);
+
+    // un jeur appuie au bon moment ?
+    if (balle_position < taille_raquette and balle_direction == 1 and digitalRead(boutonA) == 0) {
+      // le joueur A renvoie la balle
+      balle_direction = 1 - balle_direction;
+    }
+    if ((balle_position > strip.numPixels() - taille_raquette - 1) and balle_direction == 0 and digitalRead(boutonB) == 0) {
+      // le joueur B renvoie la balle
+      balle_direction = 1 - balle_direction;
+    }
+
+    // on efface la balle
+    if (balle_position < taille_raquette) {
+      // raquette verte
+      strip.setPixelColor(balle_position, strip.Color(0, 64, 0));
+    }
+    else if (balle_position > strip.numPixels() - taille_raquette - 1) {
+      // raquette rouge
+      strip.setPixelColor(balle_position, strip.Color(64, 0, 0));
+    }
+    else {
+      // en dehors de la raquette
+      strip.setPixelColor(balle_position, strip.Color(0, 0, 0));
+    }
+    strip.show();
+
+    // la balle avance d'un cran
+    if (balle_direction == 0) {
+      balle_position++;
+      balle_position++;
+      if (balle_position >= strip.numPixels()) {
+        // le joueur A a gagné !
+        scoreA++;
+        main_joueur = JOUEURA;
+        partie_en_cours = false;
+        theaterChase(strip.Color(0, 127, 0), 50); // Red
+      }
+    }
+    else {
+      balle_position--;
+      balle_position--;
+      if (balle_position == 0) {
+        // le joueur B a gagné !
+        scoreB++;
+        main_joueur = JOUEURB;
+        partie_en_cours = false;
+        theaterChase(strip.Color(127, 0, 0), 50); // Green
+      }
+    }
+
+  }
+}
+
+// Theatre-style crawling lights.
+// animation issue de la librairie de Adafruit
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, c);  //turn every third pixel on
+      }
+      strip.show();
+      delay(wait);
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
